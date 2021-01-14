35f218e08baeb4900d521e50ef5dba2a73367e64
oscar <oscarlemstra1@gmail.com>
2021-01-14 16:49:26 +0100

    h05: opdr 1 af

--- h05/opdr_1/info.php
+++ h05/opdr_1/info.php
@@ -1,3 +1,27 @@
 <?php
-    print_r($_POST);
+    $allesIngevuld = false;
+    $aantalIngevuld = 0;
+    $postLengte = count($_POST);
+
+    foreach ($_POST as $item => $value) {
+        if ($value == "") {
+            echo "Je bent je ".$item." vergeten in te vullen,"."<br>";
+            echo "ga <a href='index.html'>terug<a>.";
+            break;
+        } else {
+            $aantalIngevuld++;
+        }
+
+        if ($aantalIngevuld == $postLengte) {
+            $allesIngevuld = true;
+        }
+    }
+
+    if ($allesIngevuld == true) {
+        foreach ($_POST as $item => $value) {
+            if ($item != "wachtwoord" && $item != "knop") {
+                echo $item.": ".$value."<br><br>";
+            }
+        }
+    }
 ?>
