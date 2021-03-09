d9ec215e762d7b642f1bf72e0770d8fc6ca63146
oscar <oscarlemstra1@gmail.com>
2021-03-09 15:38:47 +0100

    h09: opdr 1 bezig

---
+++ h09/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="stylesheet.css">
+        <title>Bakkerij Wim Vlecht</title>
+    </head>
+
+    <body>
+        <header>
+            <h1>Bakkerij Vlecht Beheer</h1>
+        </header>
+
+        <div id="center-site">
+            <div id="intro">
+                <div id="buttons">
+                    <a class="button" href="index.php">Broodjes overzicht</a>
+                    <a class="button" href="">Broodje toevoegen</a>
+                </div>
+
+                <h2>Broodjes overzicht</h2>
+                <p>Informatie van de verschillende broodjes</p>
+            </div>
+
+            <div id="brood-overzicht">
+                <p>hier komt het overzicht</p>
+            </div>
+        </div>
+    </body>
+</html>
